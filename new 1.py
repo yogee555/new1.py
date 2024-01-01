963f12050d943c04d9bae52f20d4271135a0605c
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-01-01 08:25:45 +0530

    Add files via upload

---
+++ new 1.py
@@ -0,0 +1,22 @@
+# strings method
+my_string = "my_name_name_is_yogee"
+
+# find length of string using n nu
+n=len(my_string)
+print(len(my_string))
+
+# string converted in to uppercase
+print(my_string.upper())
+
+# string converted in to lowercase
+print(my_string.lower())
+
+# string find the count
+print(my_string.count("name"))
+
+# string find indexing
+print(my_string.find("yogee"))
+
+# string splitting a string
+fruit = "I like apples, mangoes, banana, grapes, cheries"
+print(fruit.split(" "))
